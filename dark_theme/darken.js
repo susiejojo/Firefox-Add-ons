f50ee34e804e86ae525e27a2c3f7dd9fb1544c9e
susiejojo <dipanwita.g@research.iiit.ac.in>
2019-04-29 18:11:05 +0530

    New Add-on

---
+++ dark_theme/darken.js
@@ -0,0 +1,9 @@
+document.body.style.backgroundColor = "black";
+document.body.style.color ="white";
+document.getElementsByTagName("main").style.backgroundColor="black";
+document.getElementById("js-repo-pjax-container").style.backgroundColor="black";
+document.getElementsByClassName("application-main ").style.backgroundColor="black";
+document.getElementsByClassName("application-main ").style.color="white";
+document.getElementsByTagName("title").style.color="white";
+
+
